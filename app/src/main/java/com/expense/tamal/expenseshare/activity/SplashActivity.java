114911455dbae0f2ce8ea002cf91f6acc2d30dce
Tamal Bhattacharya <bhattacharya.tamal@gmail.com>
2016-09-22 17:00:26 +0100

    Splash screen design

--- app/src/main/java/com/expense/tamal/expenseshare/activity/SplashActivity.java
+++ app/src/main/java/com/expense/tamal/expenseshare/activity/SplashActivity.java
@@ -1,10 +1,18 @@
 package com.expense.tamal.expenseshare.activity;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
+import com.expense.tamal.expenseshare.ExpenseShareApplication;
 import com.expense.tamal.expenseshare.R;
 
 import java.util.Timer;
@@ -12,23 +20,87 @@ import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
 
-    public static Context context;
+    private static final float ROTATE_FROM = 0.0f;
+
+    private static final float ROTATE_TO = 360.0f;
+
+    private ViewFlipper mViewFlipper;
+
+    private ImageView mProgressImage;
+
+    public static Activity context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         context = this;
+        mProgressImage = (ImageView)
+                findViewById(R.id.loading_spinner);
+        mViewFlipper = (ViewFlipper)
+                findViewById(R.id.viewFlipper);
+        startRotateAnimation(mProgressImage);
+        startFlipping();
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        if (((ExpenseShareApplication) getApplication()).isInitDone()) {
+            launchHomeScreen();
+        }
     }
 
     public static void launchHomeScreen() {
-        Timer timer = new Timer();
+        final Timer timer = new Timer();
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
                 context.startActivity(HomeActivity.getHomeActivityIntent(context));
+                timer.cancel();
+                context.finish();
             }
         };
         timer.schedule(task, 5000);
+    }
+
+    public void startRotateAnimation(ImageView imageView) {
+        Animation an = new RotateAnimation(ROTATE_FROM, ROTATE_TO,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+                0.5f);
+        an.setDuration(2000);
+        an.setInterpolator(new LinearInterpolator());
+        an.setRepeatMode(Animation.INFINITE);
+        an.setRepeatCount(200);
+        imageView.startAnimation(an);
+    }
+
+    private void startFlipping() {
+        mViewFlipper.setOutAnimation(AnimationUtils.loadAnimation(
+                this, R.anim.flip_text_out));
+        mViewFlipper.setInAnimation(AnimationUtils.loadAnimation(this,
+                R.anim.flip_text_in));
+        mViewFlipper.setFlipInterval(1000);
+        mViewFlipper.startFlipping();
+
+        mViewFlipper.getInAnimation().setAnimationListener(
+                new Animation.AnimationListener() {
+
+                    public void onAnimationStart(Animation animation) {
+                    }
+
+                    public void onAnimationRepeat(Animation animation) {
+                    }
+
+                    public void onAnimationEnd(Animation animation) {
+
+                        int displayedChild = mViewFlipper.getDisplayedChild();
+                        int childCount = mViewFlipper.getChildCount();
+
+                        if (displayedChild == childCount - 1) {
+                            mViewFlipper.stopFlipping();
+                        }
+                    }
+                });
     }
 }
